7c42566091c762f8ffcc67b505e04f7157b8c585
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-05-18 19:50:37 +0200

    fix removesong help again

--- src/Commands/1_sound/removesong.js
+++ src/Commands/1_sound/removesong.js
@@ -9,7 +9,7 @@ const helpText = `
 This command allows you to mark a song as used. This means the song will not play again.
 To use this command, send the file path of the song you want to remove from the list. You can get the file path from the \`${prefix}playable\` command.
 Keep in mind that this command does not remove the file from the server, it only marks it as used. The only way to fully remove the file is to contact the owner of the bot.
-Example usage: \`${prefix}removesong music${path.sep}users${path.sep}${message.author.id}${path.sep}mysong.mp3\`
+Example usage: \`${prefix}removesong music${path.sep}users${path.sep}$yourID${path.sep}mysong.mp3\`
 `;
 
 module.exports = new Command({
