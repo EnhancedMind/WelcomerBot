91e86b0197a6d318685fbb55a2b2a1e41e8b5537
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-05-20 00:00:29 +0200

    change ready event to clientReady for djs v15

--- src/Events/clientReady.js
+++ src/Events/clientReady.js
@@ -6,7 +6,7 @@ const { consoleLog } = require('../Data/Log');
 const { status: { status, game } } = require('../../config/config.json');
 
 
-module.exports = new Event('ready', async (client) => {
+module.exports = new Event('clientReady', async (client) => {
     consoleLog(`[INFO] ${client.user.username} is online and ready on ${client.guilds.cache.size} servers!`);
     client.user.setStatus(status);
     client.user.setActivity(
