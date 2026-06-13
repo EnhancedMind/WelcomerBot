ef5199f8d2775fee1f037d574e1101250eb88642
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-06-13 02:30:40 +0200

    fix: VoiceStateUpdate logic now correctly evaluates join and leave event with correct afk channel filtering

--- src/Events/voiceStateUpdate.js
+++ src/Events/voiceStateUpdate.js
@@ -6,18 +6,19 @@ const { getSetting } = require('../Structures/settingsManager.js');
 
 
 module.exports = new Event('voiceStateUpdate', async (client, oldState, newState) => {
-    if (oldState.member.user.bot || newState.member.user.bot) return;
-    if (newState.channelId == newState.guild.afkChannelId) return;
+    const member = newState.member || oldState.member;
+    if (!member || member.user.bot) return;
+    if (newState.channelId == newState.guild?.afkChannelId) return;
 
-    if(newState.channelId && !oldState.channelId || oldState.channelId == oldState.guild.afkChannelId) { //joining a channel or returning from afk
-        const file = await getUserSoundFile(client, newState.member.id, 'join', oldState.guild.id);
+    if(newState.channelId && (!oldState.channelId || oldState.channelId == oldState.guild?.afkChannelId)) { //joining a channel or returning from afk
+        const file = await getUserSoundFile(client, member.id, 'join', newState.guild.id);
         if (!file) return;
         client.playerManager.play(client, newState.channel, file, 800);
     }
 
-    else if(!newState.channelId && oldState.channelId) { //leaving a channel
+    else if(!newState.channelId && oldState.channelId && oldState.channelId != oldState.guild?.afkChannelId) { //leaving a channel
         if (oldState.channel.members.size == 0 && !playIntoEmptyChannel) return;
-        const file = await getUserSoundFile(client, oldState.member.id, 'leave', oldState.guild.id);
+        const file = await getUserSoundFile(client, member.id, 'leave', oldState.guild.id);
         if (!file) return;
 
         client.playerManager.play(client, oldState.channel, file, 150);
