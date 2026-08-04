2ce7e24522fccf3b3f4ecf636773dec462574904
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-08-04 18:44:35 +0200

    build: Add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js }, extends:
+        ["js/recommended"],
+        languageOptions: {
+            globals: globals.node
+        }
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            sourceType: "commonjs"
+        },
+        rules: {
+            // highlights assignments that are unnecessary or redundant
+            "no-useless-assignment": "warn",
+
+            //
+            "no-async-promise-executor": "off",
+
+            "no-empty": [
+                "warn",
+                { "allowEmptyCatch": true }
+            ],
+
+
+            // Highlights unused requires and destructured variables
+            "no-unused-vars": [
+                "warn", 
+                { 
+                    "vars": "all", 
+                    "args": "after-used", 
+                    "ignoreRestSiblings": false 
+                }
+            ],
+            // Optional: Warn about variables that are never reassigned
+            "prefer-const": "warn"
+        }
+    },
+]);
