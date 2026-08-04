76e475173f85075d3e06893b0960aea70ffb7aeb
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-08-04 02:00:47 +0200

    feat(command): Add proper argument parsing and help to user and guild commands

--- src/Commands/1_sound/guild.js
+++ src/Commands/1_sound/guild.js
@@ -1,9 +1,33 @@
 const Command = require('../../Structures/Command.js');
 
 const { PermissionsBitField } = require('discord.js');
-const { getSetting, setSetting } = require('../../Structures/settingsManager.js');
-const { bot: { ownerID }, emoji: { success, error }, response: { invalidPermissions } } = require('../../../config/config.json');
+const { parseArgs } = require('node:util');
 
+const { getSetting, setSetting } = require('../../Structures/settingsManager.js');
+const { bot: { ownerID, devIDs }, emoji: { success, error }, response: { invalidPermissions } } = require('../../../config/config.json');
+
+const helpText = `
+This command allows you to set whether the bot is enabled in the guild or not.
+You can only set the settings for the guild if you have the Manage Server permission or if you are set as a developer for the bot.
+
+There are 4 settings that can be set:
+- enabledJoin: Whether the bot is enabled for the user when they join a voice channel.
+- enabledLeave: Whether the bot is enabled for the user when they leave a voice channel.
+- enabledDefaultJoin: Whether the bot is enabled for the user when they don't have a custom join sound set.
+- enabledDefaultLeave: Whether the bot is enabled for the user when they don't have a custom leave sound set.
+
+You can set these settings using the following arguments:
+- \`--enable\` or \`-e\`: To enable the setting(s) specified by later arguments. This has priority over the \`--disable\` argument.
+- \`--disable\` or \`-d\`: To disable the setting(s) specified by later arguments.
+- \`--join\` or \`-j\`: To set the enabledJoin setting.
+- \`--leave\` or \`-l\`: To set the enabledLeave setting.
+- \`--defaultJoin\`: To set the enabledDefaultJoin setting.
+- \`--defaultLeave\`: To set the enabledDefaultLeave setting.
+- \`--all\` or \`-a\`: To set all 4 settings at once.
+- \`--reset\` or \`-r\`: To reset all 4 settings to their default values (enabled). This has priority over the \`--enable\` and \`--disable\` arguments.
+
+All the arguments are optional and additive and can be used at the same time.
+`;
 
 module.exports = new Command({
     name: 'guild',
@@ -11,38 +35,56 @@ module.exports = new Command({
     category: 'sound',
     syntax: 'guild <action> <optionalType> <optionalType> <optionalType> <optionalType>',
     description: 'Sets whether the bot is enabled in the guild or not. Requires Manage Server permission.',
+    help: helpText,
     async run(message, args, client) {
         if (!message.member.permissions.has(PermissionsBitField.Flags.ManageGuild) && message.author.id != ownerID && !devIDs.includes(message.author.id)) return await message.channel.send(`${error} ${invalidPermissions} (Manage Server)`);
 
         if (args.length > 0) {
-            const newSettings = {};
-            if ([ 'reset', 'r' ].includes(args[0])) {
-                newSettings.enabledJoin = true;
-                newSettings.enabledLeave = true;
-                newSettings.enabledDefaultJoin = true;
-                newSettings.enabledDefaultLeave = true;
+            const parsed = parseArgs({
+                args: args,
+                strict: false, // Essential for dynamic handling
+                options: {
+                    'reset': { type: 'boolean', short: 'r' },
+                    'enable': { type: 'boolean', short: 'e' },
+                    'disable': { type: 'boolean', short: 'd' },
+                    'join': { type: 'boolean', short: 'j' },
+                    'leave': { type: 'boolean', short: 'l' },
+                    'defaultJoin': { type: 'boolean' },
+                    'defaultjoin': { type: 'boolean' },
+                    'defaultLeave': { type: 'boolean' },
+                    'defaultleave': { type: 'boolean' },
+                    'all': { type: 'boolean', short: 'a' },
+                }
+            });
+
+            const flags = parsed.values;
+
+            if (flags.reset) {
+                flags.enable = true;
+                flags.all = true;
             }
-            else if (!['enable', 'en', 'disable', 'dis' ].includes(args[0])) {} // skip invalid value
-            else {
+
+            const newSettings = {};
+            if (flags.enable || flags.disable) {
                 let setting = true;
 
-                if ([ 'enable', 'en' ].includes(args[0])) {
+                if (flags.enable) {
                     setting = true;
                 }
-                else if ([ 'disable', 'dis' ].includes(args[0])) {
+                else if (flags.disable) {
                     setting = false;
                 }
 
-                if (args.includes('join') || args.includes('all')) {
+                if (flags.join || flags.all) {
                     newSettings.enabledJoin = setting;
                 }
-                if (args.includes('leave') || args.includes('all')) {
+                if (flags.leave || flags.all) {
                     newSettings.enabledLeave = setting;
                 }
-                if (args.includes('defaultJoin') || args.includes('defaultjoin') || args.includes('all')) {
+                if (flags.defaultJoin || flags.defaultjoin || flags.all) {
                     newSettings.enabledDefaultJoin = setting;
                 }
-                if (args.includes('defaultLeave') || args.includes('defaultleave') || args.includes('all')) {
+                if (flags.defaultLeave || flags.defaultleave || flags.all) {
                     newSettings.enabledDefaultLeave = setting;
                 }
             }
--- src/Commands/1_sound/user.js
+++ src/Commands/1_sound/user.js
@@ -1,8 +1,33 @@
 const Command = require('../../Structures/Command.js');
 
+const { parseArgs } = require('node:util');
+
 const { getSetting, setSetting } = require('../../Structures/settingsManager.js');
 const { bot: { ownerID, devIDs }, emoji: { success, error }, response: { invalidPermissions } } = require('../../../config/config.json');
 
+
+const helpText = `
+This command allows you to set whether the bot is enabled for a specific user or not.
+You can only set the settings for yourself unless you are set as a developer for the bot.
+
+There are 4 settings that can be set:
+- enabledJoin: Whether the bot is enabled for the user when they join a voice channel.
+- enabledLeave: Whether the bot is enabled for the user when they leave a voice channel.
+- enabledDefaultJoin: Whether the bot is enabled for the user when they don't have a custom join sound set.
+- enabledDefaultLeave: Whether the bot is enabled for the user when they don't have a custom leave sound set.
+
+You can set these settings using the following arguments:
+- \`--enable\` or \`-e\`: To enable the setting(s) specified by later arguments. This has priority over the \`--disable\` argument.
+- \`--disable\` or \`-d\`: To disable the setting(s) specified by later arguments.
+- \`--join\` or \`-j\`: To set the enabledJoin setting.
+- \`--leave\` or \`-l\`: To set the enabledLeave setting.
+- \`--defaultJoin\`: To set the enabledDefaultJoin setting.
+- \`--defaultLeave\`: To set the enabledDefaultLeave setting.
+- \`--all\` or \`-a\`: To set all 4 settings at once.
+- \`--reset\` or \`-r\`: To reset all 4 settings to their default values (enabled). This has priority over the \`--enable\` and \`--disable\` arguments.
+
+All the arguments are optional and additive and can be used at the same time.
+`;
 
 module.exports = new Command({
     name: 'user',
@@ -10,47 +35,67 @@ module.exports = new Command({
     category: 'sound',
     syntax: 'user <action> <optionalType> <optionalType> <optionalType> <optionalType> <userIDOwnerOnly>',
     description: 'Sets whether the bot is enabled for the user or not. Can only set the settings for yourself unless you are the owner of the bot.',
+    help: helpText,
     async run(message, args, client) {
         let member = message.author.id;
         for (const arg of args) {
-            const modifiedArg = arg.replace(/[<@!>]/g, '');
-            if (!isNaN(modifiedArg) && (modifiedArg.length == 18 || modifiedArg.length == 19)) {
-                member = modifiedArg;
-                if (args.length > 1 && message.author.id != ownerID && !devIDs.includes(message.author.id)) return await message.channel.send(`${error} ${invalidPermissions} (Bot owner)`);
+            const mentionMatch = arg.match((/^<@!?([0-9]{18,19})>/));
+            if (mentionMatch) {
+                if (message.author.id != ownerID && !devIDs.includes(message.author.id)) return await message.channel.send(`${error} ${invalidPermissions} (Developer)`);
+                member = mentionMatch[1];
+                // pull out this arg from the args array
+                args.splice(args.indexOf(arg), 1);
                 break;
             }
         }
 
 
-        if ( (args.length > 0 && member == message.author.id) || (args.length > 1 && member != message.author.id) ) { //only execute if there is at least one argument (args[0]) and the user is not tagged OR there is only a tagged user in args[0] 
-            const newSettings = {};
-            if ([ 'reset', 'r' ].includes(args[0])) {
-                newSettings.enabledJoin = true;
-                newSettings.enabledLeave = true;
-                newSettings.enabledDefaultJoin = true;
-                newSettings.enabledDefaultLeave = true;
+        if (args.length > 0) {
+            const parsed = parseArgs({
+                args: args,
+                strict: false, // Essential for dynamic handling
+                options: {
+                    'reset': { type: 'boolean', short: 'r' },
+                    'enable': { type: 'boolean', short: 'e' },
+                    'disable': { type: 'boolean', short: 'd' },
+                    'join': { type: 'boolean', short: 'j' },
+                    'leave': { type: 'boolean', short: 'l' },
+                    'defaultJoin': { type: 'boolean' },
+                    'defaultjoin': { type: 'boolean' },
+                    'defaultLeave': { type: 'boolean' },
+                    'defaultleave': { type: 'boolean' },
+                    'all': { type: 'boolean', short: 'a' },
+                }
+            });
+
+            const flags = parsed.values;
+
+            if (flags.reset) {
+                flags.enable = true;
+                flags.all = true;
             }
-            else if (!['enable', 'en', 'disable', 'dis' ].includes(args[0])) {} // skip invalid value
-            else {
+
+            const newSettings = {};
+            if (flags.enable || flags.disable) {
                 let setting = true;
 
-                if ([ 'enable', 'en' ].includes(args[0])) {
+                if (flags.enable) {
                     setting = true;
                 }
-                else if ([ 'disable', 'dis' ].includes(args[0])) {
+                else if (flags.disable) {
                     setting = false;
                 }
 
-                if (args.includes('join') || args.includes('all')) {
+                if (flags.join || flags.all) {
                     newSettings.enabledJoin = setting;
                 }
-                if (args.includes('leave') || args.includes('all')) {
+                if (flags.leave || flags.all) {
                     newSettings.enabledLeave = setting;
                 }
-                if (args.includes('defaultJoin') || args.includes('defaultjoin') || args.includes('all')) {
+                if (flags.defaultJoin || flags.defaultjoin || flags.all) {
                     newSettings.enabledDefaultJoin = setting;
                 }
-                if (args.includes('defaultLeave') || args.includes('defaultleave') || args.includes('all')) {
+                if (flags.defaultLeave || flags.defaultleave || flags.all) {
                     newSettings.enabledDefaultLeave = setting;
                 }
             }
