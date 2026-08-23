d8df5b9e7dcc6d86e39b5eebcc3dc95e61aee6cd
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-08-23 20:42:21 +0200

    feat(command): Make prune command use primarily bulk delete, if possible

--- src/Commands/2_admin/prune.js
+++ src/Commands/2_admin/prune.js
@@ -1,6 +1,8 @@
 const Command = require('../../Structures/Command.js');
 
 const { PermissionsBitField } = require('discord.js');
+
+const { consoleLog } = require('../../Data/Log.js');
 const { bot: { prefix, ownerID, devIDs }, emoji: { success, warning }, response: { missingArguments, invalidPermissions, invalidNumber } } = require('../../../config/config.json');
 
 module.exports = new Command({
@@ -17,29 +19,48 @@ module.exports = new Command({
         if (isNaN(args[0])) return await message.channel.send(`${warning} ${invalidNumber}`);
         if (args[0] > 100 || args[0] < 1) return await message.channel.send(`${warning} Outside of number range!`);
 
+        const response = await message.channel.send(`${success} Deleting ${args[0]} messages...`);
+
         const result = await message.channel.messages.fetch({limit: 100});
+        const resultArray = Array.from(result.values());
 
-        const resultArray = new Array();
-        result.each(message => resultArray.push(message));
+        const fourteenDays = 14 * 24 * 60 * 60 * 1000 - 25000;
+        const now = Date.now();
 
-        let messagesDeleted = 0;
-        for (let i = 0; i < resultArray.length; i++) {
+        const toBulkDelete = [];
+        const singleDeletePromises = [];
+        for (let i = 1; i < resultArray.length; i++) {
             if (resultArray[i].author.id == client.user.id) {
-                resultArray[i].delete().catch(() => {});
-                messagesDeleted++;
-                if (resultArray[i+1].content.startsWith(prefix)) {
-                    resultArray[i+1].delete().catch(() => {});
-                    messagesDeleted++;
+                if (now - resultArray[i].createdTimestamp < fourteenDays) toBulkDelete.push(resultArray[i]);
+                else singleDeletePromises.push( resultArray[i].delete().catch(() => {}) );
+
+                if (resultArray[i+1]?.content.startsWith(prefix)) {
+                    if (now - resultArray[i+1].createdTimestamp < fourteenDays) toBulkDelete.push(resultArray[i+1]);
+                    else singleDeletePromises.push( resultArray[i+1].delete().catch(() => {}) );
+                    i++; // skip next message in loop as it just got added to deletion
                 }
             }
-            if (messagesDeleted >= args[0]) break;
+
+            if (toBulkDelete.length + singleDeletePromises.length >= args[0]) break;
         }
 
-        const response = await message.channel.send(`${success} Deleting ${args[0]} messages`)
-        setTimeout(async () => {
-            // response is by bot, message is by user to trigger bot
-            response.delete().catch(() => {});
-            message.delete().catch(() => {});
-        }, 3750);
+        if (toBulkDelete.length > 0) {
+            try {
+                await message.channel.bulkDelete(toBulkDelete, true);
+            }
+            catch (error) {
+                consoleLog('Error bulk deleting from prune command:\n', error);
+                await message.channel.send(`The deletion failed. :( \`${error?.message}\``).catch(() => {});
+            }
+        }
+
+        if (singleDeletePromises.length > 0) {
+            await Promise.allSettled(singleDeletePromises);
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 3750));
+        // response is by bot, message is by user to trigger bot
+        await response.delete().catch(() => {});
+        await message.delete().catch(() => {});
     }
 });
