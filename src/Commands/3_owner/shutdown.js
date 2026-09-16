4b98efd45df782a349c4e69a9cc050b8af949916
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-09-16 15:12:22 +0200

    fix: Shutdown command blocking clean shutdown

--- src/Commands/3_owner/shutdown.js
+++ src/Commands/3_owner/shutdown.js
@@ -1,7 +1,7 @@
 const Command = require('../../Structures/Command');
 
 const { consoleLog } = require('../../Data/Log');
-const { gracefulShutdown } = require('../../utils/shutdown');
+const { gracefulShutdown, unregisterCommandExecution } = require('../../utils/shutdown');
 const { bot: { ownerID, devIDs }, emoji: { info, error }, response: { invalidPermissions } } = require('../../../config/config.json');
 
 
@@ -13,6 +13,10 @@ module.exports = new Command({
     async run(message, args, client) {
         const senderId = message.author.id;
         if (senderId != ownerID && !devIDs.includes(senderId)) return await message.channel.send(`${error} ${invalidPermissions}`);
+
+        // the command is registered in messageCreate, but it cannot unregister normally until it returns. for it to return the shutdown must finish.
+        // this creates a circular wait, thus unregistering it here fixes it
+        unregisterCommandExecution(message.id);
 
         consoleLog('[INFO] Powering off on command...');
         await message.channel.send(`${info} Shutting down...`);
