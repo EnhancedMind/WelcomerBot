6aaca015dba0a9e248bdc82199898f2d763d5e44
EnhancedMind <novotny.tomas.pribram@gmail.com>
2026-09-22 15:39:49 +0200

    feat: Add process start log

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+const { consoleLog } = require('./Data/Log');
+
+const ENV = process.env.NODE_ENV || 'development';
+consoleLog(`[INFO] Process starting... | Env: ${ENV} | PID: ${process.pid}`);
+
 const client = require('./Structures/clientInstance');
 const { setupShutdownListeners } = require('./utils/shutdown');
 
